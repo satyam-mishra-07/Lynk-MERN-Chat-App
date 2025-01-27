a5e7d3077990d7c2820da769f7ba45f5fe56842c
Satyam Mishra <112871870+satyam-mishra-07@users.noreply.github.com>
2025-01-27 14:22:56 +0530

    big fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '../../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../../frontend', 'dist', 'index.html'));
