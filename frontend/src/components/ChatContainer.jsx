76ffa37c93775511c088be69fbd651fbcd18ec58
Satyam Mishra <112871870+satyam-mishra-07@users.noreply.github.com>
2025-02-03 21:41:26 +0530

    Bug Fixed

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,12 +1,10 @@
-
 import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
-
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
-import MessageSkeleton from "./skeletons/MessageSkeleton";
-import { useAuthStore } from "../store/useAuthStore";
-import { formatMessageTime } from "../lib/utils";
+import MessageSkeleton from "./skeletons/MessageSkeleton.jsx";
+import { useAuthStore } from "../store/useAuthStore.js";
+import { formatMessageTime } from "../lib/utils.js";
 
 const ChatContainer = () => {
   const {
